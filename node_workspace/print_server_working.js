47298b6ef4447765c7001ff3d6f628e7983619fe
Tom <chanshingka1@gmail.com>
2013-03-31 21:31:05 +0800

    Update to the latest docs

--- node_workspace/print_server_working.js
+++ node_workspace/print_server_working.js
@@ -2,6 +2,7 @@ var C = require('./constants_path.js');
 
 var app = require('http').createServer(handler);
 var url = require('url');
+var qs = require('querystring');
 var fs = require("fs");
 var logger = require('./print_logger');
 
@@ -19,12 +20,14 @@ function handler (req, res) {
 	    logger.info("Body: " + body);
 	});
 
+	logger.info("Query string: " + JSON.stringify(qs.parse(url.parse(req.url).query)));
+
 	var result = '';
 	var resString = '';
 
 	switch (path)
 	{
-		case gloval.PATH_HOMEPAGE:
+		case global.PATH_HOMEPAGE:
 			logger.info("Path: " + global.PATH_HOMEPAGE);
 			fs.readFile(__dirname + '/print_simulator.html',
 				function (err, data) {
@@ -40,42 +43,48 @@ function handler (req, res) {
 		case global.PATH_LOGIN :
 			//logger.info("Path: "+global.PATH_LOGIN);
 			logger.info("Path: " + global.PATH_LOGIN);
-			result = {API_KEY:'apikey'};
+			result = {success: true, api:{key:"05A4A7F0-887E-11E2-9E96-0800200C9A66", expired_date: "2013-03-09T14:00:00.000Z"}};
 			resString = JSON.stringify(result);
 			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(resString);
 			break;
 		case global.PATH_VALIDATE_API_KEY :
 			logger.info("Path: "+global.PATH_VALIDATE_API_KEY);
-			result = {RESPONSE_TYPE: 0, RESPONSE_MSG: "Successful"};
+			result = {valid: true, msg: "successful"};
 			resString = JSON.stringify(result);
 			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(resString);
 			break;
 		case global.PATH_REQUEST_PRINT_JOBS :
 			logger.info("Path: "+global.PATH_REQUEST_PRINT_JOBS);
-			result = {PRINT_JOBS: [
+			result = {last_pid: "1234567890", print_jobs: [
 			      {
-				PID: "TEST_PID_1",
-				DOC_NAME: "printingDoc1.pdf",
-				SUBMIT_TIME: "2013-03-09T14:00:00.000Z",
-				PRINT_JOB_STATUS: 1,
-				DOC_URL: "www.ust.hk",
-				PRINTER_ID: 1,
-				COPIES: 1,
-				PAGES_PER_SHEET: 9,
-				ORIENTATION: 0
+				pid: "15A4A7F0-887E-11E2-9E96-0800200C9A66",
+				doc_name: "printingDoc1.pdf",
+				submit_time: "2013-03-09T14:00:00.000Z",
+				print_job_status: 1,
+				doc_url: "www.ustprint.com/getdocs/123456",
+				config: {
+					modifiable: true,
+					printer_id: 1,
+					copies: 1,
+					pages_per_sheet: 9,
+					orientation: 0
+				}
 			      },
 			      {
-				PID: "TEST_PID_2",
-				DOC_NAME: "printingDoc2.pdf",
-				SUBMIT_TIME: "2013-03-09T15:00:00.000Z",
-				PRINT_JOB_STATUS: 1,
-				DOC_URL: "www.ust.hk",
-				PRINTER_ID: 1,
-				COPIES: 1,
-				PAGES_PER_SHEET: 9,
-				ORIENTATION: 0
+				pid: "25A4A7F0-887E-11E2-9E96-0800200C9A66",
+				doc_name: "printingDoc2.pdf",
+				submit_time: "2013-03-09T15:00:00.000Z",
+				print_job_status: 1,
+				doc_url: "www.ustprint.com/getdocs/1234567",
+				config: {
+					modifiable: true,
+					printer_id: 1,
+					copies: 1,
+					pages_per_sheet: 9,
+					orientation: 0
+				}
 			      }
 			    ]
 			}
@@ -86,21 +95,21 @@ function handler (req, res) {
 			break;
 		case global.PATH_AMEND_PRINT_JOB :
 			logger.info("Path: "+global.PATH_AMEND_PRINT_JOB);
-			result = {RESPONSE_TYPE: 0, RESPONSE_MSG: "Successful"};
+			result = {success: true};
 			resString = JSON.stringify(result);
 			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(resString);
 			break;
 		case global.PATH_DELETE_PRINT_JOB : 
 			logger.info("Path: "+global.PATH_DELETE_PRINT_JOB);
-			result = {RESPONSE_TYPE: 0, RESPONSE_MSG: "Successful"};
+			result = {error: {code: 10, msg: "Server error"}};
 			resString = JSON.stringify(result);
 			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(resString);
 			break;
 		case global.PATH_LAUNCH_PRINT_JOB :
 			logger.info("Path: "+global.PATH_LAUNCH_PRINT_JOB);
-			result = {RESPONSE_TYPE: 0, RESPONSE_MSG: "Successful"};
+			result = {success: true};
 			resString = JSON.stringify(result);
 			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(resString);
